6e6caaa216b81cc22676a30f97cf0374b13042f8
genshibangou16 <genshibangou16.rakkan@gmail.com>
2025-06-12 14:40:35 +0900

    LoginProviderを追加し、LoginButtonをヘッダーに組み込んでMemoコンポーネントをラップ

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import Memo from './components/Memo.jsx';
+import { LoginProvider } from './hooks/LoginProvider.jsx';
+import LoginButton from './components/LoginButton.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Memo />
+    <LoginProvider>
+      <header>
+        <LoginButton />
+      </header>
+      <Memo />
+    </LoginProvider>
   </StrictMode>,
 );
