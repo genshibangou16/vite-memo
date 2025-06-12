ecc9ca324710dbb3f60a814bf3265b506e503ecd
genshibangou16 <genshibangou16.rakkan@gmail.com>
2025-06-12 13:37:37 +0900

    Memoコンポーネントのインポートパスを修正

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import Memo from '../components/Memo.jsx';
+import Memo from './components/Memo.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
