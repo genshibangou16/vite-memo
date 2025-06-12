4f7c5081165190845b7e6a822fa4e76794e78b4c
genshibangou16 <genshibangou16.rakkan@gmail.com>
2025-06-12 13:28:50 +0900

    main.jsxのインポート文を整形し、セミコロンを追加

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Memo from '../components/Memo.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import Memo from '../components/Memo.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Memo />
   </StrictMode>,
-)
+);
